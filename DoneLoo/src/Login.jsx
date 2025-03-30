22c01d5c355b59aec058d8abfe4a2c6098126d12
Nima-Morad-Nezhad <nmoradi.dev@gmail.com>
2025-03-31 01:36:04 +0300

    login client side

--- DoneLoo/src/Login.jsx
+++ DoneLoo/src/Login.jsx
@@ -6,9 +6,44 @@ axios.defaults.baseURL = "http://localhost:3000"; // ✅ Set backend base URL
 
 const Login = () => {
   return ( 
-		<div className="about"> 
-			<h1>This is the About page</h1> 
-		</div> 
+	<>
+	<div className="w-full max-w-xs">
+  <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+	<h1>DoneLoo</h1>
+  <h2 className="head-align">Login</h2>
+  
+	<div className="mb-4">
+      <label className="block text-gray-700 text-sm font-bold mb-2" for="username">
+        Email
+      </label>
+      <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="email" placeholder="Enter your Email"   />
+    </div>
+    <div className="mb-6">
+      <label className="block text-gray-700 text-sm font-bold mb-2" for="password">
+        Password
+      </label>
+      <input className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************"  />
+      <p className="text-red-500 text-xs italic">Please choose a password.</p>
+    </div>
+    <div className="flex items-center justify-between">
+      <button  className="button-design bg-sky-500 hover:bg-sky-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+        Sign In
+      </button><div className="flex flex-col">
+	  <a className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
+      I don't have an account!
+      </a>
+      <a className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"  href="/login">
+        Forgot Password?
+      </a>
+	  </div>
+	
+    </div>
+  </form>
+  <p className="text-center text-gray-500 text-xs">
+    &copy; copy right Nima!
+  </p>
+</div>
+	</>
 	) 
 } ;
 
--- DoneLoo/src/Signup.jsx
+++ DoneLoo/src/Signup.jsx
@@ -24,22 +24,19 @@ const Signup = () => {
       <label className="block text-gray-700 text-sm font-bold mb-2" for="username">
         Username
       </label>
-      <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Enter your username"   value={username}
-              onChange={(e) => setUsername(e.target.value)}/>
+      <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Enter your username"  />
     </div>
 	<div className="mb-4">
       <label className="block text-gray-700 text-sm font-bold mb-2" for="username">
         Email
       </label>
-      <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="email" placeholder="Enter your Email"   value={email}
-              onChange={(e) => setEmail(e.target.value)}/>
+      <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="email" placeholder="Enter your Email"   />
     </div>
     <div className="mb-6">
       <label className="block text-gray-700 text-sm font-bold mb-2" for="password">
         Password
       </label>
-      <input className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************"  value={password}
-              onChange={(e) => setPassword(e.target.value)}/>
+      <input className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************"  />
       <p className="text-red-500 text-xs italic">Please choose a password.</p>
     </div>
     <div className="flex items-center justify-between">
